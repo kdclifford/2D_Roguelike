8bd685fa46a9e074c1d96f6f124952f888ca3e73
Kyle Clifford <kdclifford@icloud.com>
2023-09-01 17:44:10 +0100

    Settings added

--- SourceCode/roguelike.cpp
+++ SourceCode/roguelike.cpp
@@ -1,95 +1,180 @@
-// roguelike.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-#include <time.h>
-#include <vector>
-#include "MapPoints.h"
-#include "EDirection.h"
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include "Controller.h"
+#include "Keyboard.h"
+#include "Settings.h"
 
-const int MapSize = 15;
+#define AxisDeadZone 0.05f
 
-void displayMap(const std::vector<ERoomType>& map)
+enum XboxAxis {
+	LeftStickAxis = 0,
+	RightStickAxis,
+	LeftTrigger,
+	RightTrigger,
+};
+
+
+void must_init(bool test, const char* description)
 {
-	std::cout << std::endl;
+	if (test) return;
 
-	for (int i = 0; i < map.size(); i++)
-	{
-		if (i % MapSize == 0)
-		{
-			std::cout << std::endl;
-		}
-
-		std::cout << map[i];
-	}
+	printf("couldn't initialize %s\n", description);
+	exit(1);
 }
-
-void addRandomRooms(ERoomType* map, int roomAmount, ERoomType roomType)
-{
-	ERoomType* front = map;
-	int size = MapSize * MapSize;
-
-	for (int i = 0; i < roomAmount; i++)
-	{
-		int randRoom = (rand() % size);
-		map += randRoom;
-		(*map) = roomType;
-		map = front;
-	}
-}
-
-std::vector<ERoomType> UpdateMap(const std::vector<ERoomType>& map, Cell* cell)
-{
-	std::vector<ERoomType> newMap = map;
-
-	while (cell->parent != nullptr)
-	{
-		auto position = cell->position;
-		//std::cout << endcell->position << std::endl;
-		newMap[position] = ERoomType::OptimalPath;
-		cell = cell->parent;
-
-	}
-
-	return newMap;
-}
-
-
-
 
 int main()
 {
-	const int mapOverallSize = MapSize * MapSize;
-	srand(0);
-	//ERoomType map[mapOverallSize] = { ERoomType::Empty };
-	bool edge = GetRandomNumber(0, 1);
-	bool xory = GetRandomNumber(0, 1);
-	int position = GetRandomNumber(0, MapSize - 1);
+	must_init(al_init(), "allegro");
 
-	const int endPos = GetEndPosition(edge, position, MapSize, xory);
+	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+	must_init(timer, "timer");
 
-	const int startPos = mapOverallSize % 2 == 0 ? mapOverallSize * 0.5f + (MapSize * 0.5f) : mapOverallSize * 0.5f;
-	std::vector<ERoomType> map = MazeGeneration(MapSize, startPos);
+	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+	must_init(queue, "queue");
 
-	map[startPos] = ERoomType::Start;
+	al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+	al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+	al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
 
+	ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
+	must_init(disp, "display");
+
+	ALLEGRO_FONT* font = al_create_builtin_font();
+	must_init(font, "font");
+
+	must_init(al_init_primitives_addon(), "primitives");
+
+	al_register_event_source(queue, al_get_display_event_source(disp));
+	al_register_event_source(queue, al_get_timer_event_source(timer));
+
+	bool done = false;
+	bool redraw = true;
+	ALLEGRO_EVENT event;
+
+	float x, y;
+	float i, j;
+	x = 100;
+	y = 100;
+
+	i = 100;
+	j = 100;
+
+	Settings::Settings* settings = new Settings::Settings();
+
+	KeyManager* key;
 	
-	map[endPos] = ERoomType::End;
-
-	displayMap(map);
-
-	auto endcell = AStar(map, startPos, endPos, MapSize);
-
-	if (endPos != endcell->position)
+	if (!settings->controllerType)
 	{
-		map = UpdateMap(map, AStar(map, endPos, endcell->position, MapSize, ERoomType::AmountOfRooms));
+		key = new Keyboard(queue);
+	}
+	else
+	{
+		key = new Controller(queue);
 	}
 
-	map[endcell->position] = ERoomType::OptimalPath;
+	al_start_timer(timer);
 
-	map = UpdateMap(map, endcell);
+	float joys[3][2]{0};
 
-	map[endPos] = ERoomType::End;
+	while (1)
+	{
+		al_wait_for_event(queue, &event);
 
-	displayMap(map);
+		switch (event.type)
+		{
+		case ALLEGRO_EVENT_TIMER:
+			if (key->buttonPress(EButton::Up))
+			{
+				y--;
+			}
+			if (key->buttonPress(EButton::Down))
+			{
+				y++;
+			}
+			if (key->buttonPress(EButton::Left))
+			{
+				x--;
+			}
+			if (key->buttonPress(EButton::Right))
+			{
+				x++;
+			}
+
+			done = key->exitGame();
+
+			x += joys[0][0];
+			y += joys[0][1];
+
+			i += joys[1][0];
+			j += joys[1][1];
+
+			key->resetButtonState();
+
+			redraw = true;
+			break;
+
+		case ALLEGRO_EVENT_KEY_DOWN:
+			key->buttonPress(event.keyboard.keycode, KEY_SEEN | KEY_RELEASED);
+			break;
+		case ALLEGRO_EVENT_KEY_UP:
+			key->buttonRelease(event.keyboard.keycode, KEY_RELEASED);
+			break;
+		case ALLEGRO_EVENT_JOYSTICK_BUTTON_DOWN:
+			key->buttonPress(event.joystick.button, KEY_SEEN | KEY_RELEASED);
+			break;
+		case ALLEGRO_EVENT_JOYSTICK_BUTTON_UP:
+			key->buttonRelease(event.joystick.button, KEY_RELEASED);
+			break;
+		case ALLEGRO_EVENT_JOYSTICK_AXIS:
+			
+				if (event.joystick.stick == 0 && abs(event.joystick.pos) > AxisDeadZone) { //float joys[3][2]
+					joys[event.joystick.stick][event.joystick.axis] = event.joystick.pos;
+				}
+				else if(event.joystick.stick == 0)
+				{
+					joys[event.joystick.stick][event.joystick.axis] = 0;
+				}
+			
+				if (event.joystick.stick == 1 && abs(event.joystick.pos) > AxisDeadZone) { //float joys[3][2]
+					joys[event.joystick.stick][event.joystick.axis] = event.joystick.pos;
+				}
+				else if (event.joystick.stick == 1)
+				{
+					joys[event.joystick.stick][event.joystick.axis] = 0;
+				}
+
+			break;
+
+		case ALLEGRO_EVENT_DISPLAY_CLOSE:
+			done = true;
+			break;
+		}
+
+		if (done)
+			break;
+
+		if (redraw && al_is_event_queue_empty(queue))
+		{
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 0, 0, "X: %.1f Y: %.1f", x, y);
+			al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 20, 0, "X: %.1f Y: %.1f", i, j);
+			al_draw_filled_rectangle(x, y, x + 10, y + 10, al_map_rgb(255, 0, 0));
+			al_draw_filled_rectangle(i, j, i + 10, j + 10, al_map_rgb(255, 255, 0));
+
+			al_flip_display();
+
+			redraw = false;
+		}
+	}
+
+	al_destroy_font(font);
+	al_destroy_display(disp);
+	al_destroy_timer(timer);
+	al_destroy_event_queue(queue);
+
+	return 0;
 }
